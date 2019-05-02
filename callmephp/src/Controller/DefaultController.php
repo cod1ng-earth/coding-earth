344ef8f41d2d1f996971c3580eaac5debf9c3419
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-05-02 11:53:56 +0200

    routing prefix / annotation based routing

--- callmephp/src/Controller/DefaultController.php
+++ callmephp/src/Controller/DefaultController.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
+    /**
+     * @Route("/", name="index")
+     */
     public function index(Request $request) {
         return new Response("a response by php");
     }
