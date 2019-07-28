7d3f3f3a187f806395340db8eec15a09e9ea75a9
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-07-28 22:43:54 +0200

    added kafka to coordinator coordinator dispatches events upon reception on POST /url with (url) body started writing sse channels / testing

--- coordinator/index.js
+++ coordinator/index.js
@@ -4,10 +4,14 @@ const fs = require('fs')
 const YAML = require('yaml')
 
 const {routesDef} = require('./routesDef')
+const sendMessage = require('./lib/sendMessage');
 
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 const config = require("platformsh-config").config();
 
 let PORT, routes;
@@ -24,5 +28,21 @@ if (config.isValidPlatform()) {
 }
 
 app.get('/', (req, res) => res.json(routes) )
+app.post('/url', (req, res) => {
+    const body = req.body;
+    return sendMessage(req.body, res)
+})
+
+app.get('/events', (req, res) => {
+    res.status(200).set({
+        'connection': 'keep-alive',
+        'cache-control': 'no-cache',
+        'content-type': 'text/event-stream'
+    })
+    setInterval(() => {
+        const d = (new Date).toISOString();
+        res.write(d + "\n\n");
+    }, 5000);
+});
 
 app.listen(PORT, () => console.log(`coordinator app listening on port ${PORT}!`))
