30cf5e8f4b9c7af12b7554e411ac626affef6bcb
Elisa <e.haubert@live.de>
2019-08-03 22:10:17 +0200

    rabbit vanishes in the deep of the rabbit hole

--- frontend/src/components/Rabbit.js
+++ frontend/src/components/Rabbit.js
@@ -4,23 +4,55 @@ import styled, { keyframes } from "styled-components";
 
 const move = keyframes`
 0% {
-  transform: translate(0px, 0px);
 
+  transform: translate(0px, 0px) rotate(0deg) scaleX(1) scaleY(1);
   transform-origin: center;
 }
+
+25% {
+
+  transform: translate(150px, 0px) rotate(0deg) scaleX(1) scaleY(1);
+  transform-origin: center;
+}
+
 100% {
-  transform: translate(100px, 0px);
+
+  transform: translate(150px, 0px) rotate(720deg) scaleX(0) scaleY(0);
   transform-origin: center;
 }
 `;
 
-const moveLegs = keyframes`
+
+
+const moveLegsOne = keyframes`
 0% {
   transform: rotate(-5deg);
   transform-origin: center;
 }
 100% {
-  transform: rotate(10deg);
+  transform: rotate(7deg);
+  transform-origin: center;
+}
+`;
+
+const moveLegsTwo = keyframes`
+0% {
+  transform: rotate(7deg);
+  transform-origin: center;
+}
+100% {
+  transform: rotate(-5deg);
+  transform-origin: center;
+}
+`;
+
+const moveTail = keyframes`
+0% {
+  transform: rotate(-3deg);
+  transform-origin: center;
+}
+100% {
+  transform: rotate(3deg);
   transform-origin: center;
 }
 `;
@@ -36,11 +68,22 @@ z-index: 1000;
     display: none;
   }
 
-animation: ${move} infinite 2s ease-in;
+animation: ${move} infinite 5s linear;
 
-#upper-right-leg-sitting, #upper-left-leg-sitting, #lower-left-leg-sitting, #lower-right-leg-sitting {
-  animation: ${moveLegs} infinite 1s linear;
+
+
+
+#upper-right-leg-sitting, #upper-left-leg-sitting {
+  animation: ${moveLegsOne} infinite 1s linear;
   }
+
+ #lower-left-leg-sitting, #lower-right-leg-sitting {
+    animation: ${moveLegsTwo} infinite 1s linear;
+    }
+
+  #tail {
+       animation: ${moveTail} infinite 1s linear;
+       }
 
 `;
 
