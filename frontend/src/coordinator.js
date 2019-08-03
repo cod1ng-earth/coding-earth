53bd83f9be51c60030f30f55e2dccaf2a24c32a7
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-08-03 14:08:15 +0200

    when running frontend against a remote coordinator, http -> https are killing your frontend xhr. this helps it
    Signed-off-by: Stefan Adolf <stefan.adolf@turbinekreuzberg.com>

--- frontend/src/coordinator.js
+++ frontend/src/coordinator.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const endpoint = process.env.REACT_APP_COORDINATOR ?
-    "//" + process.env.REACT_APP_COORDINATOR :
+    process.env.REACT_APP_COORDINATOR :
     "//coordinator." + window.location.host
 
 export const coordinator = axios.get(endpoint)
