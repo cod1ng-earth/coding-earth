e1696739a2c1a6b6bf304b2558e7f8f68d4881dc
markusphil <m.philipp1991@web.de>
2019-08-03 18:38:57 +0200

    remplace search

--- rabbithole/index.js
+++ rabbithole/index.js
@@ -1,23 +1,22 @@
-require('dotenv').config()
+require("dotenv").config();
 const config = require("platformsh-config").config();
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const logger = require('./lib/logger')
-const Actions = require('./actions/index');
-const search = require('./routes/search');
-const hello = require('./routes/hello');
+const logger = require("./lib/logger");
+const Actions = require("./actions/index");
+const search = require("./routes/search");
+const hello = require("./routes/hello");
 
 const app = express();
 app.use(logger.middleware);
 app.use(cors());
 app.use(express.json());
 
-const PORT = config.isValidPlatform() ?  config.port : (process.env.PORT || 3000);
+const PORT = config.isValidPlatform() ? config.port : process.env.PORT || 3000;
 
-app.get('/', search);
-app.get('/hello', hello);
+app.get("/", hello);
 
-app.listen(PORT, () => logger.app.info(`rabbithole app listening on port ${PORT}!`))
-
-
+app.listen(PORT, () =>
+  logger.app.info(`rabbithole app listening on port ${PORT}!`)
+);
