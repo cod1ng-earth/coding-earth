2dbf46cb9aa3b8c2a92a41b23532d67ec453b547
Elisa <e.haubert@live.de>
2019-08-03 22:42:24 +0200

    rabbits legs and head move different. also, it jumps now...

--- frontend/src/components/Rabbit.js
+++ frontend/src/components/Rabbit.js
@@ -9,15 +9,21 @@ const move = keyframes`
   transform-origin: center;
 }
 
+15% {
+
+  transform: translate(110px, -20px) rotate(0deg) scaleX(1) scaleY(1);
+  transform-origin: center;
+}
+
 25% {
 
-  transform: translate(150px, 0px) rotate(0deg) scaleX(1) scaleY(1);
+  transform: translate(180px, 0px) rotate(0deg) scaleX(1) scaleY(1);
   transform-origin: center;
 }
 
 100% {
 
-  transform: translate(150px, 0px) rotate(720deg) scaleX(0) scaleY(0);
+  transform: translate(180px, 0px) rotate(720deg) scaleX(0) scaleY(0);
   transform-origin: center;
 }
 `;
@@ -29,8 +35,13 @@ const moveLegsOne = keyframes`
   transform: rotate(-5deg);
   transform-origin: center;
 }
-100% {
+50% {
   transform: rotate(7deg);
+  transform-origin: center;
+}
+
+100% {
+  transform: rotate(-5deg);
   transform-origin: center;
 }
 `;
@@ -40,10 +51,16 @@ const moveLegsTwo = keyframes`
   transform: rotate(7deg);
   transform-origin: center;
 }
-100% {
+50% {
   transform: rotate(-5deg);
   transform-origin: center;
 }
+
+100% {
+  transform: rotate(7deg);
+  transform-origin: center;
+}
+
 `;
 
 const moveTail = keyframes`
@@ -57,12 +74,46 @@ const moveTail = keyframes`
 }
 `;
 
+const moveEars = keyframes`
+0% {
+  transform: translate(0px, 0px);
+  transform-origin: center;
+}
+50% {
+  transform: translate(0px, 30px);
+  transform-origin: center;
+}
+
+100% {
+  transform: translate(0px, 0px);
+  transform-origin: center;
+}
+`;
+
+const moveFace = keyframes`
+0% {
+  transform: translate(0px, 20px);
+  transform-origin: center;
+}
+50% {
+  transform: translate(0px, -10px);
+  transform-origin: center;
+}
+
+100% {
+  transform: translate(0px, 20px);
+  transform-origin: center;
+}
+`;
+
+
+
 const StyledRabbit = styled(Rabbit)`
 height: 85px;
 width: 100px;
 position: absolute;
 top: 145px;
-right: 350px;
+right: 380px;
 z-index: 1000;
   #carrot {
     display: none;
@@ -70,20 +121,24 @@ z-index: 1000;
 
 animation: ${move} infinite 5s linear;
 
-
-
-
-#upper-right-leg-sitting, #upper-left-leg-sitting {
+#upper-right-leg-sitting, #lower-left-leg-sitting {
   animation: ${moveLegsOne} infinite 1s linear;
   }
 
- #lower-left-leg-sitting, #lower-right-leg-sitting {
+  #upper-left-leg-sitting, #lower-right-leg-sitting {
     animation: ${moveLegsTwo} infinite 1s linear;
     }
 
   #tail {
        animation: ${moveTail} infinite 1s linear;
        }
+
+#left-ear, #right-ear {
+  animation: ${moveEars} infinite 1s linear;
+}
+#face, #eyes, #nose {
+  animation: ${moveFace} infinite 1s linear;
+}
 
 `;
 
