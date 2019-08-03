c5be29ba2bb0b1e47be0173a034a740a3ce11dbc
markusphil <m.philipp1991@web.de>
2019-08-03 18:08:23 +0200

    log urls

--- rabbithole/lib/logger.js
+++ rabbithole/lib/logger.js
@@ -1,24 +1,20 @@
-const {createLogger, format,transports} = require('winston');
-const expressWinston = require('express-winston');
+const { createLogger, format, transports } = require("winston");
+const expressWinston = require("express-winston");
 
 module.exports = {
-    app: createLogger({
-        format:
-            format.combine(
-                format.timestamp({
-                    format: 'YYYY-MM-DD HH:mm:ss'
-                }),
-            ),
-        transports: [
-            new transports.Console({
-                level: 'debug'
-            }),
-        ]
-    }),
-    middleware: expressWinston.logger({
-        transports: [
-            new transports.Console()
-        ],
-        format: format.json()
-    })
-}
+  app: createLogger({
+    format: format.combine(
+      format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss"
+      })
+    ),
+    transports: [
+      new transports.Console({
+        level: "debug"
+      })
+    ]
+  }),
+  middleware: expressWinston.logger({
+    transports: [new transports.Console()]
+  })
+};
