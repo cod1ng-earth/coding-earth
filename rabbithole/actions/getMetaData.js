5d8bafa046e15ac58ec31bcd8e71093f8bd15953
Julian <yuri-mail@posteo.de>
2019-08-04 00:44:19 +0200

    fix minor bug

--- rabbithole/actions/getMetaData.js
+++ rabbithole/actions/getMetaData.js
@@ -4,11 +4,11 @@ const getFavicons = require('get-website-favicon')
 const getMetaData = async url => {
   getFavicons(url).then(data => {
     const iconUrlFavicon = data.icons ? data.icons[0].src : null;
-    console.log("ICON URL: " + iconUrl);
+    console.log("ICON URL: " + iconUrlFavicon);
     return {
       iconUrlFavicon,
     };
-  });
+  }).catch(console.log);
 };
 
 module.exports = getMetaData;
