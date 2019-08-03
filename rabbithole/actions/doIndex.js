2c6e0833f2f8772aed362b56844f88270007365e
markusphil <m.philipp1991@web.de>
2019-08-03 17:43:47 +0200

    remove twitter stuff

--- rabbithole/actions/doIndex.js
+++ rabbithole/actions/doIndex.js
@@ -1,24 +1,19 @@
-const elastic = require('../lib/elasticsearch');
-const logger = require('../lib/logger');
+const elastic = require("../lib/elasticsearch");
+const logger = require("../lib/logger");
 
-const index = async ({type, url, content}) => {
-    if (type !== 'tweet')
-        return false;
-    try {
-        const result = await elastic.index({
-            index: 'tweets',
-            id: content.id_str,
-            type: 'tweet',
-            body: content,
-        })
-        return result;
-    } catch(e) {
-        logger.app.error(e);
-        return false;
-    }
+const index = async ({ type, url, content }) => {
+  if (type !== "carrot") return false;
+  try {
+    const result = await elastic.index({
+      index: "carrot",
+      type: "carrot",
+      url: url
+    });
+    return result;
+  } catch (e) {
+    logger.app.error(e);
+    return false;
+  }
+};
 
-}
-
-module.exports = index
-
-
+module.exports = index;
