455de7f5ee914f9d10353bb1aa8ae58760edcdd4
Elisa <e.haubert@live.de>
2019-08-03 21:40:07 +0200

    the rabbit learned to walk... straight into the rabbit hole

--- frontend/src/components/Rabbit.js
+++ frontend/src/components/Rabbit.js
@@ -2,17 +2,46 @@ import React from "react";
 import { ReactComponent as Rabbit } from "./../images/hasi.svg";
 import styled, { keyframes } from "styled-components";
 
-const pulse = keyframes``;
+const move = keyframes`
+0% {
+  transform: translate(0px, 0px);
+
+  transform-origin: center;
+}
+100% {
+  transform: translate(100px, 0px);
+  transform-origin: center;
+}
+`;
+
+const moveLegs = keyframes`
+0% {
+  transform: rotate(-5deg);
+  transform-origin: center;
+}
+100% {
+  transform: rotate(10deg);
+  transform-origin: center;
+}
+`;
 
 const StyledRabbit = styled(Rabbit)`
 height: 85px;
 width: 100px;
 position: absolute;
 top: 145px;
-left: 100px;
+right: 350px;
+z-index: 1000;
   #carrot {
     display: none;
   }
+
+animation: ${move} infinite 2s ease-in;
+
+#upper-right-leg-sitting, #upper-left-leg-sitting, #lower-left-leg-sitting, #lower-right-leg-sitting {
+  animation: ${moveLegs} infinite 1s linear;
+  }
+
 `;
 
 export default props => {
