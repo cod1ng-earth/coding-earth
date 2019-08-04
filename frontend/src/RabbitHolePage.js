f27e75768dda4902af49a36056a6f80877b77414
Kristin Fritsch <ich@kristin-fritsch.de>
2019-08-04 11:57:36 +0200

    Add carrots

--- frontend/src/RabbitHolePage.js
+++ frontend/src/RabbitHolePage.js
@@ -8,42 +8,62 @@ import { ReactComponent as Watch } from "./images/watch.svg";
 
 import { coordinator, endpoint } from "./coordinator";
 
-export default props => {
-  const [knownServices, setServices] = useState({});
+const CarrotBin = props => {
+  console.log(props.carrots);
+  const carrots = props.carrots.map(carrot => {
+    return (
+      <li class="carrot" key={carrot.url} style={{ padding: "40px" }}>
+        <a href="{carrot.url}" target="_blank">
+          <img
+            style={{ backgroundColor: "#fff", borderRadius: "50%" }}
+            src={carrot.favicon}
+          />
+        </a>
+      </li>
+    );
+  });
+  return <ul>{carrots}</ul>;
+};
 
-  useEffect(() => {
-    async function fetchData() {
-      const routes = await coordinator;
-      setServices(routes.data);
-    }
-    fetchData();
+export default class RabbitHolePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carrots: []
+    };
+  }
 
+  componentDidMount() {
     const eventSource = new EventSource(`${endpoint}/events`);
     eventSource.onmessage = msg => {
       if ("ping" === msg.data) return false;
 
-      const message = JSON.parse(msg.data);
-      console.log("Welcome to rabbit hole");
-      console.log(message);
-      eventEmitter.emit(`content-${message.type}`, message);
-    };
-  }, []);
+      const carrot = JSON.parse(msg.data);
 
-  return (
-    <div
-      style={{
-        backgroundColor: "#310f3f",
-        height: "100%",
-        minHeight: "500px",
-        color: "#FFF",
-        padding: "20px"
-      }}
-    >
-      <Fortuneteller />
-      <Funghi />
-      <Funghi2 />
-      <Card />
-      <Watch />
-    </div>
-  );
-};
+      let carrots = this.state.carrots;
+      carrots.push(carrot);
+      this.setState({ carrots: carrots });
+    };
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          backgroundColor: "#310f3f",
+          height: "100%",
+          minHeight: "500px",
+          color: "#FFF",
+          padding: "20px"
+        }}
+      >
+        <CarrotBin carrots={this.state.carrots} />
+        <Fortuneteller />
+        <Funghi />
+        <Funghi2 />
+        <Card />
+        <Watch />
+      </div>
+    );
+  }
+}
