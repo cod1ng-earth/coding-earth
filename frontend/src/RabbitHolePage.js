f4912c9555715fa1658957f6763332393072a9b8
Kristin Fritsch <ich@kristin-fritsch.de>
2019-08-04 12:23:06 +0200

    url

--- frontend/src/RabbitHolePage.js
+++ frontend/src/RabbitHolePage.js
@@ -24,7 +24,7 @@ const CarrotBin = props => {
           padding: "20px"
         }}
       >
-        <a href={carrot.url} target="_blank">
+        <a href={"/" + carrot.url} target="_blank">
           <img
             style={{ backgroundColor: "#fff", borderRadius: "50%" }}
             src={carrot.favicon}
@@ -66,97 +66,100 @@ export default class RabbitHolePage extends React.Component {
 
   render() {
     return (
+      <div
+        style={{
+          backgroundColor: "#310f3f",
+          height: "100%",
+          minHeight: "1550px",
+          color: "#FFF",
+          padding: "20px"
+        }}
+      >
+        <div
+          style={{
+            margin: "0 auto",
+            marginTop: "40px"
+          }}
+        >
+          <p
+            style={{
+              fontSize: "30px",
+              fontWeight: "bold",
+              textAlign: "center",
+              letterSpacing: "2px",
+              color: "#e5d3ed"
+            }}
+          >
+            WELCOME TO THE RABBIT HOLE
+          </p>
+        </div>
+
+        <a href="">
+          <div
+            style={{
+              width: "250px",
+              position: "absolute",
+              left: "50%",
+              top: "200px",
+              marginLeft: "-125px"
+            }}
+          >
+            <Fortuneteller />
+          </div>
+        </a>
 
         <div
           style={{
-            backgroundColor: "#310f3f",
-            height: "100%",
-            minHeight: "1550px",
-            color: "#FFF",
-            padding: "20px"
+            position: "absolute",
+            width: "220px",
+            right: "-70px"
           }}
         >
-          <div
-            style={{
-              margin: "0 auto",
-              marginTop:"40px"
-            }}
-          >
-            <p style={{
-              fontSize:"30px",
-              fontWeight:"bold",
-              textAlign:"center",
-              letterSpacing:"2px",
-              color:"#e5d3ed"
-            }}>WELCOME TO THE RABBIT HOLE</p>
-          </div>
+          <Funghi />
+        </div>
 
-          <a href="">
-            <div
-              style={{
-                width: "250px",
-                position:"absolute",
-                left:"50%",
-                top:"200px",
-                marginLeft:"-125px"
-              }}
-            >
-              <Fortuneteller />
-            </div>
-          </a>
+        <div
+          style={{
+            position: "absolute",
+            width: "250px",
+            left: "-70px"
+          }}
+        >
+          <Funghi2 />
+        </div>
 
-          <div
-            style={{
-              position: "absolute",
-              width: "220px",
-              right: "-70px"
-            }}
-          >
-            <Funghi />
-          </div>
+        <div
+          style={{
+            position: "absolute",
+            width: "250px",
+            right: "-70px",
+            top: "600px"
+          }}
+        >
+          <Card />
+        </div>
 
-          <div
-            style={{
-              position: "absolute",
-              width: "250px",
-              left: "-70px"
-            }}
-          >
-            <Funghi2 />
-          </div>
+        <div
+          style={{
+            position: "absolute",
+            width: "450px",
+            left: "270px",
+            top: "800px"
+          }}
+        >
+          <Watch />
+        </div>
 
-          <div
-            style={{
-              position: "absolute",
-              width: "250px",
-              right: "-70px",
-              top: "600px"
-            }}
-          >
-            <Card />
-          </div>
-
-          <div
-            style={{
-              position: "absolute",
-              width: "450px",
-              left: "270px",
-              top: "800px"
-            }}
-          >
-            <Watch />
-          </div>
-
-          <div
-            style={{
-              position: "absolute",
-              width: "150px",
-              left: "-30px",
-              top: "1300px"
-            }}
-          >
-            <Rabbit />
-          </div>
+        <div
+          style={{
+            position: "absolute",
+            width: "150px",
+            left: "-30px",
+            top: "1300px"
+          }}
+        >
+          <Rabbit />
+        </div>
 
         <CarrotBin carrots={this.state.carrots} />
       </div>
