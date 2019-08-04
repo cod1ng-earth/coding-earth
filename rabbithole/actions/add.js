b58b6ce6c694af11ac4fd93260336031d81b55a7
markusphil <m.philipp1991@web.de>
2019-08-04 08:59:30 +0200

    fix regex

--- rabbithole/actions/add.js
+++ rabbithole/actions/add.js
@@ -13,7 +13,7 @@ const add = async value => {
   console.log(`${value.url} goes into the rabbithole`);
 
   const validUrl = value.url.match(
-    "^(?:http(s)?://)?[w.-]+(?:.[w.-]+)+[w-._~:/?#[]@!$&'() *+,;=.]+$"
+    "/((([A-Za-z]{3,9}:(?://)?)(?:[-;:&=+$,w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,w]+@)[A-Za-z0-9.-]+)((?:/[+~%/.w-_]*)???(?:[-+=&;%@.w_]*)#?(?:[.!/\\w]*))?)/"
   );
 
   if (validUrl) {
