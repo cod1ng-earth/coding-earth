e7fb42bacb4d1e80b7689283b78caa226267e535
Julian <yuri-mail@posteo.de>
2019-08-04 09:48:53 +0200

    return promise

--- rabbithole/actions/getMetaData.js
+++ rabbithole/actions/getMetaData.js
@@ -1,7 +1,7 @@
 const getFavicons = require("get-website-favicon");
 
 const getMetaData = url => {
-  getFavicons(url)
+  return getFavicons(url)
     .then(data => {
       let iconUrlFavicon;
       if (data.icons && data.icons[0] && data.icons[0].src) {
