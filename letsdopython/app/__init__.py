4c62732e116c63b84b5d7056340eb38294904468
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-08-04 11:42:57 +0200

    yield es result int != str in python
    Signed-off-by: Stefan Adolf <stefan.adolf@turbinekreuzberg.com>

--- letsdopython/app/__init__.py
+++ letsdopython/app/__init__.py
@@ -17,19 +17,24 @@ if config.is_valid_platform():
     port = int(config.port)
     esCredentials = config.credentials('elasticsearch')
     print(esCredentials)
-    esHost = "http://" + esCredentials['host'] + ":" + esCredentials['port']
+    esHost = "http://" + esCredentials['host'] + ":" + str(esCredentials['port'])
 else:
     port = int(os.getenv('PORT', 3000))
     esHost = os.getenv('ELASTICSEARCH_HOST', 'http://localhost:9288')
 
 app = Flask(__name__)
 
+elasticsearch = Elasticsearch(esHost)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World yay!"
+    res = elasticsearch.search(index="youtube", body={"query": {"match_all": {}}})
+    print(res)
 
+    print("%d documents found" % res['hits']['total'])
+    js0n = json.dumps( res['hits']['hits'])
+    return js0n
 
 if __name__ == "app":
     http_server = gevent.pywsgi.WSGIServer(('127.0.0.1', port), app)
